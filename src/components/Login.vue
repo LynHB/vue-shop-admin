da71d7d1300b87819ab8ca70704020cc64824b25
LynHB <huan9_bin9@163.com>
2020-12-28 22:29:06 +0800

    初始化

---
+++ src/components/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    登录组件
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+}
+</script>
+
+<!--
+  scoped : 仅当前模块生效
+  lang="less" : 支持less语法
+-->
+<style lang="less" scoped>
+
+</style>
